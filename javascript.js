be99d1092a448e525cfd72517af110372ac06fcf
Fefe <mynameisfefe@protonmail.com>
2021-01-12 03:28:58 -0300

    can change div

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 let gridwidth = 16;
 let gridheight = 16;
-
+let currentwidth =16;
 for (i=0; i< gridwidth * gridheight; i++) {
   const cuadradoGrande = document.querySelector('#cuadradoGrande');
   const lesserGrid = document.createElement ('div');
@@ -8,15 +8,23 @@ for (i=0; i< gridwidth * gridheight; i++) {
   cuadradoGrande.appendChild(lesserGrid);
 };
 function grid (gridwidth, gridheight) {
-  for (i=0; i< gridwidth * gridheight; i++) {
-    const cuadradoGrande = document.querySelector('#cuadradoGrande');
-    const lesserGrid = document.createElement ('div');
-    lesserGrid.classList.add("cuadradoChico");
-    lesserGrid.style.width =  32*16/gridwidth-2  + 'px';
-    lesserGrid.style.height =  32*16/gridwidth-2 + 'px';
-    cuadradoGrande.appendChild(lesserGrid);
+  if (currentwidth==gridwidth) {
+  let fefe = 4984;}
+  else {
+    document.querySelectorAll('.cuadradoChico').forEach(function(a){
+      a.remove()
+      })
+    for (i=0; i< gridwidth * gridheight; i++) {
+      const cuadradoGrande = document.querySelector('#cuadradoGrande');
+      const lesserGrid = document.createElement ('div');
+      lesserGrid.classList.add("cuadradoChico");
+      lesserGrid.style.width =  32*16/gridwidth-2  + 'px';
+      lesserGrid.style.height =  32*16/gridwidth-2 + 'px';
+      cuadradoGrande.appendChild(lesserGrid);
+    }
   }
 }
+
 const changecolor = document.querySelector("div");
 changecolor.addEventListener ("mouseover", function(e) {
   e.target.style.background ="blue";
@@ -27,6 +35,7 @@ button.addEventListener ('click', function (a) {
   eraseColor.forEach (div => {
     div.style.background ="rgb(27, 158, 27)";
   });
+   
   let gridwidth = prompt ("How many grids would you like?");
   gridheight = gridwidth;
   grid (gridwidth,gridheight);
